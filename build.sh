bca540d8e482428e2b0c3612388ac99ebba7b093
Sylvain Noiry <snoiry@kalrayinc.com>
2025-08-27 17:48:49 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -11,10 +11,9 @@ cd build
 # increase max number of open files for linking
 ulimit -n 2048
 
-#-DLLVM_EXTERNAL_LIT=$LLVM_BUILD_DIR/bin/llvm-lit \
-
 cmake -G Ninja \
   -DMLIR_DIR=$LLVM_PREFIX/lib/cmake/mlir \
+  -DLLVM_EXTERNAL_LIT=$(which lit) \
   -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
   -DCMAKE_C_COMPILER=/usr/bin/clang \
   -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
@@ -25,10 +24,13 @@ cmake -G Ninja \
   -DPython_FIND_VIRTUALENV=ONLY \
   -DPython3_FIND_VIRTUALENV=ONLY \
   -Dpybind11_DIR=`pybind11-config --cmakedir` \
+  -DLLVM_ENABLE_LLD=On \
+  -DLLVM_BUILD_LLVM_DYLIB=ON \
+  -DLLVM_LINK_LLVM_DYLIB=ON \
+  -DMLIR_LINK_MLIR_DYLIB=ON \
+  -DMLIR_BUILD_MLIR_C_DYLIB=ON \
   -DCMAKE_BUILD_TYPE=Debug \
   ..
 
-cmake --build .
 cmake --build . --target check-myproject
-cmake --install .
 
