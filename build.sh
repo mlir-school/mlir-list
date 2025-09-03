5d7f809a513e2d37a08c01f06841909944aeb14c
Sylvain Noiry <snoiry@kalrayinc.com>
2025-09-03 17:33:59 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-source ./mlir_venv/bin/activate
-
 mkdir -p install
 INSTALL_DIR=$(realpath ./install)
 
-mkdir build
+mkdir -p build
 cd build
 
 # increase max number of open files for linking
 ulimit -n 2048
+
+LLVM_PREFIX=$(python -c 'import mlir;print(mlir.__path__[0])')
 
 cmake -G Ninja \
   -DMLIR_DIR=$LLVM_PREFIX/lib/cmake/mlir \
@@ -17,9 +17,6 @@ cmake -G Ninja \
   -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
   -DCMAKE_C_COMPILER=/usr/bin/clang \
   -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
-  -DCMAKE_EXE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
-  -DCMAKE_MODULE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
-  -DCMAKE_SHARED_LINKER_FLAGS_INIT="-fuse-ld=lld" \
   -DMLIR_ENABLE_BINDINGS_PYTHON=ON \
   -DPython_FIND_VIRTUALENV=ONLY \
   -DPython3_FIND_VIRTUALENV=ONLY \
@@ -27,8 +24,6 @@ cmake -G Ninja \
   -DLLVM_ENABLE_LLD=On \
   -DLLVM_BUILD_LLVM_DYLIB=ON \
   -DLLVM_LINK_LLVM_DYLIB=ON \
-  -DMLIR_LINK_MLIR_DYLIB=ON \
-  -DMLIR_BUILD_MLIR_C_DYLIB=ON \
   -DCMAKE_BUILD_TYPE=Debug \
   ..
 
