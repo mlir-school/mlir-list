6106d8d568c8aca4fb0462b86472d2f12337cc8b
Sylvain Noiry <sylvain.noiry@hotmail.fr>
2025-09-04 18:20:36 +0200

    Remove old scripts

--- create_venv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python3 -m venv mlir_venv
-source mlir_venv/bin/activate
-python -m pip install --upgrade pip
-
-pip install numpy
-pip install pybind11
-
--- tosource.sh
+++ tosource.sh
@@ -1,22 +1,3 @@
-# VENV
-
-if [ -d "./mlir_venv" ]; then
-  source ./mlir_venv/bin/activate
-fi
-
-# LLVM-PROJECT
-
-export PATH=$(realpath ./llvm-project/build/bin):$PATH
-
-# llvm-project build dir
-export LLVM_BUILD_DIR="$(realpath ./llvm-project/build)"
-# llvm-project install dir
-export LLVM_PREFIX="$(realpath ./llvm-project/install)"
-# mlir python bindings
-export PYTHONPATH=$(realpath ./llvm-project/install/python_packages/mlir_core):$PYTHONPATH
-
-# MYPROJECT
-
 # myproject binaries
 export PATH=$(realpath ./build/bin):$PATH
 
