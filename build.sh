68c97a1c7240f4b06d23df3b3d26dd91b54c2245
Sylvain Noiry <sylvain.noiry@hotmail.fr>
2025-09-04 18:33:03 +0200

    Clean build script

--- build.sh
+++ build.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
 
-mkdir -p install
-INSTALL_DIR=$(realpath ./install)
-
 mkdir -p build
 cd build
 
 # increase max number of open files for linking
 ulimit -n 2048
 
-LLVM_PREFIX=$(python -c 'import mlir;print(mlir.__path__[0])')
+LLVM_PREFIX=$(python3 -c 'import mlir;print(mlir.__path__[0])')
 
 cmake -G Ninja \
   -DMLIR_DIR=$LLVM_PREFIX/lib/cmake/mlir \
   -DLLVM_EXTERNAL_LIT=$(which lit) \
-  -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
   -DCMAKE_C_COMPILER=/usr/bin/clang \
   -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
   -DMLIR_ENABLE_BINDINGS_PYTHON=ON \
-  -DPython_FIND_VIRTUALENV=ONLY \
-  -DPython3_FIND_VIRTUALENV=ONLY \
   -Dpybind11_DIR=`pybind11-config --cmakedir` \
   -DLLVM_ENABLE_LLD=On \
   -DLLVM_BUILD_LLVM_DYLIB=ON \
   -DLLVM_LINK_LLVM_DYLIB=ON \
   -DCMAKE_BUILD_TYPE=Debug \
+  -Wno-dev \
   ..
 
 cmake --build . --target check-myproject
