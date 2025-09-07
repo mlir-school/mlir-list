2a4346d2a5d8b02c5c29b1518ca3941b4cd18fc9
Sylvain Noiry <sylvain.noiry@hotmail.fr>
2025-09-08 00:11:33 +0200

    Clean for ex3

--- python/ListProjectExtensionNanobind.cpp
+++ python/ListProjectExtensionNanobind.cpp
@@ -25,22 +25,6 @@ NB_MODULE(_listprojectDialectsNanobind, m) {
   //===--------------------------------------------------------------------===//
   auto listM = m.def_submodule("list");
 
-  //===-------------------------------------------------------------------===//
-  // ListType
-  //===-------------------------------------------------------------------===//
-
-  auto mlirListType =
-      mlir_type_subclass(m, "ListType", mlirTypeIsAListType);
-
-  mlirListType.def_classmethod(
-      "get",
-      [](const nb::object &cls, MlirContext ctx, MlirType element_type) {
-        return cls(mlirListTypeGet(ctx, element_type));
-      },
-      "Gets an instance of ListType in the same context", nb::arg("cls"),
-      nb::arg("ctx"),
-      nb::arg("element_type"));
-
   listM.def(
       "register_dialect",
       [](MlirContext context, bool load) {
