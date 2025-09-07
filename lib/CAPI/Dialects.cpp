42272cd840cf2d5a129919ae441febca3c61f787
Sylvain Noiry <sylvain.noiry@hotmail.fr>
2025-09-07 23:58:25 +0200

    Clean for ex5

--- lib/CAPI/Dialects.cpp
+++ lib/CAPI/Dialects.cpp
@@ -17,15 +17,4 @@ using namespace mlir;
 MLIR_DEFINE_CAPI_DIALECT_REGISTRATION(List, list,
                                       mlir::list::ListDialect)
 
-//===-------------------------------------------------------------------===//
-// ListType
-//===-------------------------------------------------------------------===//
-
-bool mlirTypeIsAListType(MlirType type) {
-  return isa<list::ListType>(unwrap(type));
-}
-
-MlirType mlirListTypeGet(MlirContext ctx, MlirType elementType) {
-  return wrap(list::ListType::get(unwrap(ctx), unwrap(elementType)));
-}
 
